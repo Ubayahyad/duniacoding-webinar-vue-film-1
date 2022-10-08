1354e0b10b278d6d3292fbac33ee86e4293286c6
septe habudin <septe.vhirgo@gmail.com>
2022-10-08 20:04:34 +0700

    install package tailwindcss, setup tailwind css, and testing if tailwind is ready

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+// add this
+import "./style.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/FilmView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl font-bold text-red-500 underline ">
+    Hello world!
+  </h1>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
